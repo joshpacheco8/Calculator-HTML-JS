0e37798580761f9990fc585067bc77ef87c9775d
Josue Pacheco <jgpachecor08@gmail.com>
2023-01-11 23:11:17 -0600

    first commit

---
+++ js/Calculadora.js
@@ -0,0 +1,25 @@
+const mod = 100;
+class Calculadora
+{
+    
+    sumar(num1, num2)
+    {
+        return num1+num2;
+    }
+    restar(num1, num2)
+    {
+        return num1-num2;
+    }
+    multiplicar(num1, num2)
+    {
+        return num1*num2;
+    }
+    dividir(num1, num2)
+    {
+        return num1/num2;
+    }
+    modulo(num1, mod)
+    {
+        return num1/mod;
+    }
+}
